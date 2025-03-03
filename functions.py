490ddbef11c69621a37aa028cedb6e933baab64d
adambisa <adambisa611@gmail.com>
2025-03-03 22:52:59 +0100

    add basic functionality for adding heroes and help function

--- functions.py
+++ functions.py
@@ -1,11 +1,58 @@
 import pyfiglet
 from rich import print
+from random import choice
+import json
 
+classes = ["barbarian", "bard", "cleric", "druid",
+           "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "wizard", "warlock"]
 
 def help_dnd():
-    title = pyfiglet.figlet_format('Hello', font='isometric2')
-    print(f'[yellow]{title}[/yellow]')
-
+    title = pyfiglet.figlet_format('SpellbookCLI', font="doom")
+    print(f'[red]{title}[/red]')
+    print(f'Welcome Adventurers, warm around my fire and tell me a tale.')
+    print(f'Usage : python3 main.py [-h|help] OR [-f|fill] [-n|name "someName"] OR [-i|init] OR [-u|update] OR [-p|prepare] OR [-c|color]') 
+    all_monsters = ["Goblins", "Dragons", "Mindflayers", "Cultists", "Demons", "Devils", "Beasts", "Giants", "Gods"]
+    monsters = choice(all_monsters)
+    print(f'Hope this was enough of an explanation, now get ready to slay some [red]{monsters}[/red] !')
 
 def initialize():
-    print("")
+    name = input("Choose a name for your hero: ")
+    class_dnd = input("Which class are you playing? ")
+    if class_dnd.lower() not in classes:
+        print("[red]Invalid class supplied. Roll for initiative. [/red]")
+    lvl_dnd = input("Which lvl are you? ")
+    bonus = input("What is your bonus to your spellcasting modifier? ")
+    try:
+        int(lvl_dnd)
+    except ValueError:
+        print("[red]Invalid level supplied. Roll for initiative. [/red]")
+        return
+    finally:
+        lvl_dnd = int(lvl_dnd)
+    try:
+        int(bonus)
+    except ValueError:
+        print("[red]Invalid ability modifier supplied. Roll for initiative. [/red]")
+        return
+    finally:
+        bonus = int(bonus)
+    if lvl_dnd > 20:
+        print("[red]Level supplied is too high, perhaps you had too much homebrew. [/red]")
+        return
+        
+        
+    num_of_prepared_spells = lvl_dnd + bonus
+
+    data = {'name': name, 'class': class_dnd, 'lvl':lvl_dnd, 'modifier':bonus, 'total_spells': num_of_prepared_spells, 'spells' : [], 'cantrips':[]}
+    with open(f'{name}.json', 'w') as f:
+        json.dump(data, f) 
+    print(f"[green]The spellbook for hero {name} has been initialized. Save travels, wanderer.[/green]")
+    print(f"[green]Feel free to proceed and fill your spellbook, so you can prepare spells later.[/green]")
+
+
+def fill_spellbook(name:str):
+    """
+    open shit by name check lvl, provide options for spells from json i get from api really easy
+    """
+    with open(f"{name}.json", "r") as f:
+        print(f"{f.read()}")
--- main.py
+++ main.py
@@ -12,8 +12,7 @@ from functions import *
 
 base_url = "https://www.dnd5eapi.co/"
 
-classes = ["barbarian", "bard", "cleric", "druid",
-           "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "wizard", "warlock"]
+
 
 
 args = sys.argv
@@ -26,13 +25,21 @@ there needs to be a certain amount of args, ill just add shit here over time
 i am thinking something like  py main.py [init|-i] OR [prep|-p] OR [update|-u] OR [help|-h]
 """
 
-
-match args[1]:
-    case "-i":
-        initialize()
-    case "init":
-        initialize()
-    case "-h":
-        help_dnd()
-    case "help":
-        help_dnd()
+if len(args) == 2 :
+    match args[1]:
+        case "-i":
+            initialize()
+        case "init":
+            initialize()
+        case "-h":
+            help_dnd()
+        case "help":
+            help_dnd()
+elif len(args) == 3:
+    match args[1]:
+        case "-n":
+            fill_spellbook(args[2])
+        case "name":
+            fill_spellbook(args[2])
+else:
+    help_dnd()
